c0a5b0a5b39c97b7f2b5f3ef1166a3974de66dcb
Your Name <severus.key308@gmail.com>
2022-11-06 23:42:03 +0400

    2nd Com

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,22 @@
-var character = 'mario';
+var character = 'carlos';
 console.log(character);
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
 });
+// -----------------------------------------------  Type Basics
+var karacter = 'james';
+var age = 30;
+var isBlackBelt = false;
+// karacter = 20;
+karacter = 'naomi';
+// age = 'james';
+age = 40;
+// isBlackBelt = 50;
+isBlackBelt = true;
+// ------------
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(9));
+// -----------------------------------------------  Objects & Arrays
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,4 @@
-const character = 'mario';
+const character = 'carlos';
 
 console.log(character);
 
@@ -6,4 +6,32 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
   console.log(input);
-});
+});  
+
+
+// -----------------------------------------------  Type Basics
+
+let karacter = 'james';
+let age = 30;
+let isBlackBelt = false;
+
+// karacter = 20;
+karacter = 'naomi';
+
+// age = 'james';
+age = 40;
+
+// isBlackBelt = 50;
+isBlackBelt = true;
+
+// ------------
+const circ = (diameter: number) => {
+   return diameter * Math.PI
+}
+
+console.log(circ(9));
+
+
+// -----------------------------------------------  Objects & Arrays
+
+
